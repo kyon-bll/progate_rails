c8c7865d9acd33de395ed1887c5b5e897fb959ff
kyosuke <kyosuke.araki611@gmail.com>
2018-05-23 23:42:35 +0900

    lesson 2, 3

--- app/controllers/ruby_controller.rb
+++ app/controllers/ruby_controller.rb
@@ -1,5 +1,22 @@
+# coding: utf-8
+
+require "date"
+
 class RubyController < ApplicationController
+  class Character
+    attr_accessor :name
+    attr_accessor :price
+
+    def initialize(name, price)
+      self.name = name
+      self.price = price
+    end
+    
+  end
+
   def index
-    puts "Hello Ruby"
+    pizza = Character.new('pizza', 400)
+      
+    @result = Date.today.wednesday?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # coding: utf-8
 Rails.application.routes.draw do
   
-  get 'posts/index'
+  get 'posts/new'
+  post 'posts/create'
+  get 'posts/:id' => 'posts#show'
+  get 'posts' => 'posts#index'
+
   get '/' => 'home#top'
   get 'about' => 'home#about'
 
