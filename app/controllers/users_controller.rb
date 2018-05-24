20497ebb5626b7bf74e5301546c7becca52e54e6
kyosuke <kyosuke.araki611@gmail.com>
2018-05-24 23:36:17 +0900

    Lesson 6

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+# coding: utf-8
+class UsersController < ApplicationController
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users")
+    else
+      render("users/new")
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました"
+      redirect_to("/users")
+    else
+      render("users/edit")
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+
+    flash[:notice] = "ユーザーを削除しました"
+    redirect_to("/users")
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # coding: utf-8
 Rails.application.routes.draw do
   
+  get 'users' => 'users#index'
+  get 'signup' => 'users#new'
+  post 'users/create'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
+  post 'users/:id/destroy' => 'users#destroy'
+  get 'users/:id' => 'users#show'
+
   get 'posts' => 'posts#index'
   get 'posts/new'
   post 'posts/create'
@@ -14,5 +22,6 @@ Rails.application.routes.draw do
 
   # progate ruby 用
   get 'ruby/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
